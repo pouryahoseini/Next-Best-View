d708285787246e508d526b9d72360687309ad44b
Pourya Hoseini <shoseinialinodehi@ucsd.edu>
2021-03-16 22:15:34 -0700

    Overhauled the surface normal computation functions. Also added a separate function to fetch depth maps from files.

--- src/surface_normal.py
+++ src/surface_normal.py
@@ -2,106 +2,68 @@ import numpy as np
 import cv2 as cv
 
 
-def compute_surface_normal(test_number, tile_number, random_test):
-    # Read the file
-    if (test_number % 30) >= 15:
-        filename = "./TestImages/" + str(int(test_number / 30)) + "/Front_Down.npy"
-        random_filename = "./TestImages/" + str(random_test) + "/Front_Down.npy"
-    else:
-        filename = "./TestImages/" + str(int(test_number / 30)) + "/Front_Up.npy"
-        random_filename = "./TestImages/" + str(random_test) + "/Front_Up.npy"
+def compute_average_surface_normal_depth(depth_map):
+    """
+    Compute average length of the object surface normal's image on z-axis (depth axis) for all the pixels that lay on the object (z-axis is the perpendicular to the image plane).
+    :param depth_map: Input the depth map
+    :return: Average depth of the surface normal for all the pixels on the object surface (float)
+    """
 
-    # Load the numpy file
-    depth_image = np.load(filename)
-    random_depth_image = np.load(random_filename)
+    # Replace any nan values with the maximum value in the tile (thus set them as background)
+    max_depth = np.max(depth_map)
+    depth_map[np.where(((depth_map == 0) | (depth_map == np.nan)))] = max_depth
 
-    # Modify the depth image based on the alterations in the rgb image
-    variation = test_number % 15
-    mean_depth = np.mean(depth_image)
+    # Segment the depth map via Otsu threshold method
+    converted_depth_map = np.floor((depth_map / max_depth) * 255).astype(np.uint8)
+    _, segmented_map = cv.threshold(converted_depth_map, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
 
-    whiteout_blackout_ratio = 0.5
-    superimpose_ratio = 0.5
+    # For any point on object surface (foreground)
+    foreground_count = normal_depth_sum = 0
+    for i in range(1, depth_map.shape[0] - 1):
+        for j in range(1, depth_map.shape[1] - 1):
+            if segmented_map[i, j] == 255:
 
-    if int(depth_image.shape[0] * superimpose_ratio) > random_depth_image.shape[0]:
-        half_row = random_depth_image.shape[0]
-    else:
-        half_row = int(depth_image.shape[0] * superimpose_ratio)
+                # Compute horizontal and vertical depth derivatives
+                dzdx = depth_map[i + 1, j] - depth_map[i - 1, j]
+                dzdy = depth_map[i, j + 1] - depth_map[i, j - 1]
 
-    if int(depth_image.shape[1] * superimpose_ratio) > random_depth_image.shape[1]:
-        half_col = random_depth_image.shape[1]
-    else:
-        half_col = int(depth_image.shape[1] * superimpose_ratio)
+                # Compute the surface normal
+                pixel_surface_normal = cv.normalize(np.array([dzdx, dzdy, 1]), dst=None)
 
-    if variation == 1:  # Top whiteout
-        depth_image[: int(depth_image.shape[0] * whiteout_blackout_ratio), :] = mean_depth
-    elif variation == 2:  # Bottom whiteout
-        depth_image[int(depth_image.shape[0] * (1 - whiteout_blackout_ratio)):, :] = mean_depth
-    elif variation == 3:  # Left blackout
-        depth_image[:, : int(depth_image.shape[1] * whiteout_blackout_ratio)] = mean_depth
-    elif variation == 4:  # Right blackout
-        depth_image[:, int(depth_image.shape[1] * (1 - whiteout_blackout_ratio)):] = mean_depth
-    elif variation == 7:  # Superimpose, top left
-        depth_image[: half_row, : half_col] = random_depth_image[random_depth_image.shape[0] - half_row:, random_depth_image.shape[1] - half_col:]
-    elif variation == 8:  # Superimpose, top right
-        depth_image[: half_row, depth_image.shape[1] - half_col:] = random_depth_image[random_depth_image.shape[0] - half_row:, : half_col]
-    elif variation == 9:  # Superimpose, bottom left
-        depth_image[depth_image.shape[0] - half_row:, : half_col] = random_depth_image[: half_row, random_depth_image.shape[1] - half_col:]
-    elif variation == 10:  # Superimpose, bottom right
-        depth_image[depth_image.shape[0] - half_row:, depth_image.shape[1] - half_col:] = random_depth_image[: half_row, : half_col]
+                # Dot product with the z-axis (depth axis) by choosing only the z (depth) component of the surface normal
+                normal_depth_sum += pixel_surface_normal[2]
 
-    # Select the tile
-    height = depth_image.shape[0]
-    width = depth_image.shape[1]
-    if tile_number == 1:
-        tile = depth_image[: int(height / 3), : int(width / 3)]
-    elif tile_number == 2:
-        tile = depth_image[: int(height / 3), int(width / 3): int(2 * width / 3)]
-    elif tile_number == 3:
-        tile = depth_image[: int(height / 3), int(2 * width / 3):]
-    elif tile_number == 4:
-        tile = depth_image[int(height / 3): int(2 * height / 3), : int(width / 3)]
-    elif tile_number == 5:
-        tile = depth_image[int(height / 3): int(2 * height / 3), int(2 * width / 3):]
+                # Keep account of the foreground pixels
+                foreground_count += 1
 
-    # Replace any nan values with the maximum value in the tile
-    max_value = np.max(tile)
-    tile[np.where(((tile == 0) | (tile == np.nan)))] = max_value
-
-    # Segment the depth tile via Otsu's threshold
-    tile_image = np.floor((tile / max_value) * 255).astype(np.uint8)
-    # cv.imshow("original", tile_image)
-    # cv.waitKey(0)
-
-    _, segmented_image = cv.threshold(tile_image, 0, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
-    # cv.imshow("segmented", segmented_image)
-    # cv.waitKey(0)
-
-    # Compute the surface normal at any point and normalize
-    # normal_image = np.zeros_like(tile)
-    count = 0
-    z_normal_sum = 0
-    dummy_normilize = np.zeros(3)
-    for i in range(1, tile.shape[0] - 1):
-        for j in range(1, tile.shape[1] - 1):
-            if segmented_image[i, j] == 255:
-                dzdx = tile[i + 1, j] - tile[i - 1, j]
-                dzdy = tile[i, j + 1] - tile[i, j - 1]
-                pixel_surface_normal = cv.normalize(np.array([-dzdx, -dzdy, 0.01]), dummy_normilize)
-                z_normal_sum += pixel_surface_normal[2]
-                count += 1
-
-            # normal_image[i,j] = pixel_surface_normal[2]
-
-    # Take the average of surface normals in the tile
-    # and compute the dot product with the z-axis by choosing only the z component of the surface normals
-    if count > 0:
-        average_normal_z = z_normal_sum / count
+    # Take the average of surface normals in the depth map
+    if foreground_count > 0:
+        average_normal_z = normal_depth_sum / foreground_count
     else:
         average_normal_z = 0
 
-    # normal_image_conv = np.floor((normal_image / np.max(normal_image)) * 255).astype(np.uint8)
-    # cv.imshow("normal", normal_image_conv)
-    # cv.waitKey(0)
-    # print(average_normal_z)
-
     return average_normal_z
+
+
+def get_depth_map(test_number, random_test):
+    """
+    Read the frontal and random depth maps.
+    :param test_number: The current test number (int)
+    :param random_test: Random test to superimpose part of the front image with the random one
+
+    :return: Frontal depth map, random test map
+    """
+
+    # Find the file addresses to read
+    if (test_number % 30) >= 15:
+        frontal_filename = "./test_set/" + str(int(test_number / 30)) + "/Front_Down.npy"
+        random_filename = "./test_set/" + str(random_test) + "/Front_Down.npy"
+    else:
+        frontal_filename = "./test_set/" + str(int(test_number / 30)) + "/Front_Up.npy"
+        random_filename = "./test_set/" + str(random_test) + "/Front_Up.npy"
+
+    # Load the depth maps
+    frontal_depth_image = np.load(frontal_filename)
+    random_depth_image = np.load(random_filename)
+
+    return frontal_depth_image, random_depth_image
