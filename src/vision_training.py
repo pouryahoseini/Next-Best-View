18be44f82c3a38332621b3ee316838e99da5727e
Pourya Hoseini <shoseinialinodehi@ucsd.edu>
2021-05-05 19:53:08 -0700

    Training of ResNet 101 was added as a classification method

--- src/vision_training.py
+++ src/vision_training.py
@@ -31,7 +31,7 @@ def train_vision(configurations):
     augmented_dataset_images, augmented_dataset_labels = train_augmentation.train_data_augmentation(dataset_images, dataset_labels)
 
     # In the case of Dempster-Shafer fusion, add the 'Universal' class to the dataset
-    if configurations["decision_fusion_type"] == 'DST':
+    if configurations["decision_fusion_type"] == 'Dempster-Shafer':
         dataset_images, dataset_labels, label_codes = create_dempster_shafer_dataset(dataset_images, dataset_labels, label_codes,
                                                                                      augmented_dataset_images=augmented_dataset_images,
                                                                                      augmented_dataset_labels=augmented_dataset_labels,
@@ -42,7 +42,7 @@ def train_vision(configurations):
         dataset_labels = augmented_dataset_labels
 
     # Shuffle the data
-    shuffled_indices = np.choice(dataset_labels.shape[0], size=dataset_labels.shape[0], replace=False)
+    shuffled_indices = np.random.permutation(dataset_labels.shape[0])
     dataset_images = dataset_images[shuffled_indices]
     dataset_labels = dataset_labels[shuffled_indices]
 
@@ -52,6 +52,10 @@ def train_vision(configurations):
 
     # Repeat for all tiles and the original images
     for tile_name, tile_no in [('original', 0)] + [('tile_' + str(tile_no), tile_no) for tile_no in range(1, 6)]:
+
+        # Create save directory on drive if it does not exist
+        if not os.path.exists(os.path.join('./model/', tile_name)):
+            os.mkdir(os.path.join('./model/', tile_name))
 
         # Extract the tiles from the dataset
         dataset_tiles = extract_tile(dataset_images=dataset_images, tile_no=tile_no)
@@ -112,11 +116,11 @@ def train_model(dataset_images, dataset_labels, configurations, save_directory):
 
         if 'HOG' in configurations["svm_feature_types"]:
             # Train PCA feature reduction
-            pca = feature_engineering.pca_train(features_dataset=hog_features, number_of_features=configurations["hog_reduced_features_no"], save_directory=save_directory)
+            feature_engineering.pca_train(features_dataset=hog_features, number_of_features=configurations["hog_reduced_features_no"], save_directory=save_directory)
 
             # Reduce HOG features
-            pca_projector = feature_engineering.PCAProjector()
-            hog_features = pca_projector.pca_project(sample=hog_features, pca=pca)
+            pca_projector = feature_engineering.PCAProjector(load_dir=save_directory)
+            hog_features = pca_projector.pca_project(sample=hog_features)
 
         # Concatenate the feature vectors
         dataset_features = np.concatenate((hog_features, color_hist_features, hu_moments_features), axis=1)
@@ -156,18 +160,28 @@ def train_model(dataset_images, dataset_labels, configurations, save_directory):
     elif configurations["classifier_type"] == 'NN':
 
         # Train the convolutional neural network
-        model = convolutional_neural_network(image_dataset=dataset_images,
-                                             label_dataset=dataset_labels,
-                                             network_type=configurations["nn_network_architecture"],
-                                             save_directory=save_directory,
-                                             nn_epochs=configurations["nn_epochs"],
-                                             nn_max_learning_rate=configurations["nn_max_learning_rate"],
-                                             nn_batch_size=configurations["nn_batch_size"],
-                                             nn_validation_split=configurations["nn_validation_split"],
-                                             nn_early_stopping_patience=configurations["nn_early_stopping_patience"])
+        if configurations["nn_network_architecture"] == 'resnet101':
+            model = resnet101(image_dataset=dataset_images,
+                              label_dataset=dataset_labels,
+                              save_directory=save_directory,
+                              nn_epochs=configurations["nn_epochs"],
+                              nn_max_learning_rate=configurations["nn_max_learning_rate"],
+                              nn_batch_size=configurations["nn_batch_size"],
+                              nn_validation_split=configurations["nn_validation_split"],
+                              nn_early_stopping_patience=configurations["nn_early_stopping_patience"])
+        else:
+            model = convolutional_neural_network(image_dataset=dataset_images,
+                                                 label_dataset=dataset_labels,
+                                                 network_type=configurations["nn_network_architecture"],
+                                                 save_directory=save_directory,
+                                                 nn_epochs=configurations["nn_epochs"],
+                                                 nn_max_learning_rate=configurations["nn_max_learning_rate"],
+                                                 nn_batch_size=configurations["nn_batch_size"],
+                                                 nn_validation_split=configurations["nn_validation_split"],
+                                                 nn_early_stopping_patience=configurations["nn_early_stopping_patience"])
 
     else:
-        raise Exception("Classifier type ' + classifier_type + ' not recognized.")
+        raise Exception("Classifier type " + configurations["classifier_type"] + " not recognized.")
 
     return model
 
@@ -202,7 +216,7 @@ def convolutional_neural_network(image_dataset, label_dataset, network_type, sav
     input_layer = tf.keras.layers.Input(shape=image_dataset.shape[1:])
 
     # Decide on the network architecture
-    if network_type == 1:
+    if network_type == '1':
 
         # Network 1: Convolutional layers
         ln_0 = tf.keras.layers.LayerNormalization(axis=[1, 2, 3])(input_layer)
@@ -244,7 +258,7 @@ def convolutional_neural_network(image_dataset, label_dataset, network_type, sav
 
         output_layer = tf.keras.layers.Dense(units=label_dataset_one_hot.shape[1], activation='softmax', kernel_initializer='glorot_normal', activity_regularizer=None, use_bias=True)(activation_6)
 
-    elif network_type == 2: # Difference with Net 1: tanh instead of relu, average pooling instead of max pooling
+    elif network_type == '2':   # Difference with Net 1: tanh instead of relu, average pooling instead of max pooling
 
         # Network 2: Convolutional layers
         ln_0 = tf.keras.layers.LayerNormalization(axis=[1, 2, 3])(input_layer)
@@ -286,7 +300,7 @@ def convolutional_neural_network(image_dataset, label_dataset, network_type, sav
 
         output_layer = tf.keras.layers.Dense(units=label_dataset_one_hot.shape[1], activation='softmax', kernel_initializer='glorot_normal', activity_regularizer=None, use_bias=True)(activation_6)
 
-    elif network_type == 3: # Difference with Net 1: One less dropout, CNN, one max pooling layers at the end of the convolutional layers
+    elif network_type == '3':   # Difference with Net 1: One less dropout, CNN, one max pooling layers at the end of the convolutional layers
 
         # Network 3: Convolutional layers
         ln_0 = tf.keras.layers.LayerNormalization(axis=[1, 2, 3])(input_layer)
@@ -322,10 +336,10 @@ def convolutional_neural_network(image_dataset, label_dataset, network_type, sav
         output_layer = tf.keras.layers.Dense(units=label_dataset_one_hot.shape[1], activation='softmax', kernel_initializer='glorot_normal', activity_regularizer=None, use_bias=True)(activation_5)
 
     else:
-        raise ValueError("The CNN network type is not recognized.")
+        raise ValueError("The CNN network type " + network_type + " is not recognized.")
 
-    # Save the trained classifier
-    file_address = os.path.join(save_directory, 'CNN.h5')
+    # Set the file name for saving the trained classifier
+    file_address = os.path.join(save_directory, 'CNN_' + str(network_type) + '.h5')
 
     # Define a learning rate scheduler
     def schedule(epoch, lr):
@@ -360,6 +374,80 @@ def convolutional_neural_network(image_dataset, label_dataset, network_type, sav
     return model
 
 
+def resnet101(image_dataset, label_dataset, save_directory, nn_epochs, nn_max_learning_rate, nn_batch_size, nn_validation_split, nn_early_stopping_patience):
+    """
+    Train a Resnet 101 with transfer learning and frozen convolutional layers.
+    :param image_dataset: Input image dataset (4D array: sample_no, row, column, channel)
+    :param label_dataset: Input label dataset (1D array)
+    :param save_directory: Directory to save the trained model on drive (str)
+    :param nn_epochs: Number of epochs to train the neural network (int)
+    :param nn_max_learning_rate: Maximum learning rate during training of the neural network (float)
+    :param nn_batch_size: Mini-batch size during training (int)
+    :param nn_validation_split: Validation split of the training data (float between 0 and 1)
+    :param nn_early_stopping_patience: Patience in epochs of early stopping of training (int)
+    :return: The trained convolutional neural network model
+    """
+
+    # Adjust the range of input values to the proper expected range of Resnet 101
+    image_dataset = tf.keras.applications.resnet.preprocess_input(image_dataset)
+
+    # Create categorical one-hot labels for the network output
+    label_dataset_one_hot = tf.keras.utils.to_categorical(label_dataset)
+
+    # Shuffle data
+    shuffled_indices = np.random.permutation(image_dataset.shape[0])
+    image_dataset = image_dataset[shuffled_indices]
+    label_dataset_one_hot = label_dataset_one_hot[shuffled_indices]
+
+    # Define the pre-trained convolutional layers of Resnet 101
+    resnet = tf.keras.applications.ResNet101(include_top=False, weights='imagenet', input_shape=image_dataset.shape[1:])
+
+    # Freeze the pre-trained convolutional layers
+    for layer in resnet.layers:
+        layer.trainable = False
+
+    # Add the fully-connected layers on top of the Resnet convolutional layers
+    average_pool = tf.keras.layers.GlobalAveragePooling2D()(resnet.layers[-1].output)
+    flatten = tf.keras.layers.Flatten()(average_pool)
+    dense_1 = tf.keras.layers.Dense(200, activation='relu')(flatten)
+    dense_2 = tf.keras.layers.Dense(label_dataset_one_hot.shape[1], activation='softmax')(dense_1)
+
+    # Set the file name for saving the trained classifier
+    file_address = os.path.join(save_directory, 'Resnet.h5')
+
+    # Define a learning rate scheduler
+    def schedule(epoch, lr):
+        if epoch < (nn_epochs / 4):
+            return nn_max_learning_rate
+        elif epoch < (nn_epochs / 2):
+            return nn_max_learning_rate / 3.0
+        elif epoch < (3 * nn_epochs / 4):
+            return nn_max_learning_rate / 10.0
+        else:
+            return nn_max_learning_rate / 20.0
+    learning_rate_scheduler = tf.keras.callbacks.LearningRateScheduler(schedule)
+
+    # Construct the NN model
+    model = tf.keras.Model(inputs=resnet.inputs, outputs=dense_2)
+
+    # Define the learning optimizer
+    optimizer = tf.keras.optimizers.Adam(learning_rate=nn_max_learning_rate)
+
+    # Compile the model
+    model.compile(loss="categorical_crossentropy", optimizer=optimizer, metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
+
+    # Set model checkpoints so that the best model is saved
+    model_checkpoint = tf.keras.callbacks.ModelCheckpoint(filepath=file_address, monitor="val_loss", save_best_only=True, save_weights_only=False)
+
+    # Define early stopping conditions and returning the best model after finishing the training
+    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=nn_early_stopping_patience, restore_best_weights=True)
+
+    # Train the NN model
+    model.fit(image_dataset, label_dataset_one_hot, epochs=nn_epochs, batch_size=nn_batch_size, validation_split=nn_validation_split, shuffle=True, callbacks=[model_checkpoint, learning_rate_scheduler, early_stopping])
+
+    return model
+
+
 def random_forest(feature_dataset, label_dataset, save_directory, rf_criterion, rf_estimators_no, cross_validation_splits):
     """
     Train a Random Forest Classifier.
@@ -373,7 +461,7 @@ def random_forest(feature_dataset, label_dataset, save_directory, rf_criterion, 
     """
 
     # Normalize the dataset
-    feature_dataset = train_normalizer(feature_dataset)
+    feature_dataset = train_normalizer(feature_dataset, save_directory, 'Normalizer_RF')
 
     # Make the label vector a 1D array by unraveling
     label_dataset = label_dataset.ravel()
@@ -386,18 +474,18 @@ def random_forest(feature_dataset, label_dataset, save_directory, rf_criterion, 
 
     # Set grid search parameters
     param_grid = dict(max_depth=(None, 50, 100), min_samples_split=(5, 10), min_samples_leaf=(1, 3))
-    grid_of_classifiers = sklearn.model_selection.GridSearchCV(rf_to_be_optimized, param_grid=param_grid, scoring=['accuracy', 'recall_macro', 'precision_macro', 'neg_log_loss'], refit='neg_log_loss', cv=cross_validation_settings, n_jobs=-1, verbose=3)
+    refit_metric = 'neg_log_loss'
+    grid_of_classifiers = sklearn.model_selection.GridSearchCV(rf_to_be_optimized, param_grid=param_grid, scoring=['accuracy', 'recall_macro', 'precision_macro', 'neg_log_loss'], refit=refit_metric, cv=cross_validation_settings, n_jobs=-1, verbose=3)
 
     # Perform grid search to find the best parameters for the random forest classifier
     grid_of_classifiers.fit(feature_dataset, label_dataset)
 
-    # Use the best parameters to train the classifier on the whole training data
-    rf_classifier = sklearn.ensemble.RandomForestClassifier(n_estimators=rf_estimators_no, criterion=rf_criterion, class_weight='balanced', n_jobs=-1, **grid_of_classifiers.best_estimator_)
-    rf_classifier.fit(feature_dataset, label_dataset)
+    # Keep the best estimator
+    rf_classifier = grid_of_classifiers.best_estimator_
 
     # Print the best found parameters and the best score
-    print('\n\nBest Accuracy: ' + str(grid_of_classifiers.best_score_))
-    print('Best Parameters: \n{}\n\n'.format(grid_of_classifiers.best_params_))
+    print('\n\nBest ' + refit_metric + ': ' + str(grid_of_classifiers.best_score_))
+    print('Best Parameters: {}\n'.format(grid_of_classifiers.best_params_))
 
     # Save the trained classifier
     file_address = os.path.join(save_directory, 'RF.pkl')
@@ -419,7 +507,7 @@ def support_vector_machine(feature_dataset, label_dataset, save_directory, svm_k
     """
 
     # Normalize the dataset
-    feature_dataset = train_normalizer(feature_dataset)
+    feature_dataset = train_normalizer(feature_dataset, save_directory, 'Normalizer_SVM')
 
     # Make the label vector a 1D array by unraveling
     label_dataset = label_dataset.ravel()
@@ -435,21 +523,21 @@ def support_vector_machine(feature_dataset, label_dataset, save_directory, svm_k
     svm_to_be_optimized = sklearn.svm.SVC(probability=True, cache_size=cache_size, class_weight=class_weight, decision_function_shape='ovr', kernel=svm_kernel)
 
     # Set grid search parameters
-    c_range = np.logspace(0, 1, 3)
-    gamma_range = np.logspace(0, 1, 3)
-    param_grid = dict(gamma=gamma_range, C=c_range)
-    grid_of_classifiers = sklearn.model_selection.GridSearchCV(svm_to_be_optimized, param_grid=param_grid, scoring=['accuracy', 'recall_macro', 'precision_macro', 'neg_log_loss'], refit='neg_log_loss', cv=cross_validation_settings, n_jobs=-1, verbose=3)
+    c_range = np.logspace(-1, 1, 3)
+    kernels = ['poly', 'rbf']
+    param_grid = dict(kernel=kernels, C=c_range)
+    refit_metric = 'neg_log_loss'
+    grid_of_classifiers = sklearn.model_selection.GridSearchCV(svm_to_be_optimized, param_grid=param_grid, scoring=['accuracy', 'recall_macro', 'precision_macro', 'neg_log_loss'], refit=refit_metric, cv=cross_validation_settings, n_jobs=-1, verbose=3)
 
     # Find the optimal classifier parameters (C and Gamma)
     grid_of_classifiers.fit(feature_dataset, label_dataset)
 
-    # Use the best parameters found to train the SVM classifier on the whole training data
-    svm_classifier = sklearn.svm.SVC(probability=True, cache_size=cache_size, class_weight=class_weight, decision_function_shape='ovr', kernel=svm_kernel, **grid_of_classifiers.best_params_)
-    svm_classifier.fit(feature_dataset, label_dataset)
+    # Keep the best estimator
+    svm_classifier = grid_of_classifiers.best_estimator_
 
     # Print the best found parameters and the best score
-    print('\n\nBest Accuracy: ' + str(grid_of_classifiers.best_score_))
-    print('Best Parameters: \n{}\n\n'.format(grid_of_classifiers.best_params_))
+    print('\n\nBest ' + refit_metric + ': ' + str(grid_of_classifiers.best_score_))
+    print('Best Parameters: {}\n'.format(grid_of_classifiers.best_params_))
 
     # Save the trained classifier
     file_address = os.path.join(save_directory, 'SVM.pkl')
@@ -459,10 +547,12 @@ def support_vector_machine(feature_dataset, label_dataset, save_directory, svm_k
     return svm_classifier
 
 
-def train_normalizer(dataset):
+def train_normalizer(dataset, save_directory, save_name=None):
     """
     Train a dataset normalizer, save it on drive, and return the normalized dataset.
     :param dataset: Input dataset (2D array: sample_no, features)
+    :param save_directory: Directory to save the normalizer (str)
+    :param save_name: Name of the saved normalizer on drive (str)
     :return: Normalized dataset
     """
 
@@ -473,7 +563,9 @@ def train_normalizer(dataset):
     normalized_dataset = normalizer.fit_transform(dataset)
 
     # Save the normalizer on disk
-    with open('./model/Normalizer.pkl', 'wb') as normalizer_file:
+    if save_name is None:
+        save_name = 'Normalizer'
+    with open(os.path.join(save_directory, save_name + '.pkl'), 'wb') as normalizer_file:
         pickle.dump(normalizer, normalizer_file)
 
     return normalized_dataset
@@ -515,7 +607,8 @@ def create_dempster_shafer_dataset(dataset_images, dataset_labels, label_codes, 
     dst_dataset_labels = np.concatenate((augmented_dataset_labels, np.ones(universal_class_sample_no, dtype=augmented_dataset_labels.dtype) * label_codes.shape[0]), axis=0)
 
     # Add the universal class label code
-    dst_label_codes = np.append(label_codes, 'Universal')
+    # dst_label_codes = np.append(label_codes, 'Universal')
+    dst_label_codes = label_codes
 
     return dst_dataset_images, dst_dataset_labels, dst_label_codes
 
@@ -530,7 +623,7 @@ def get_train_samples(root_address, image_size, train_images_extension):
     """
 
     # List the directories in the root address
-    list_of_folders = [folder for folder in os.listdir(root_address) if os.path.isdir(folder)]
+    list_of_folders = [folder for folder in os.listdir(root_address) if os.path.isdir(os.path.join(root_address, folder))]
 
     # Initialize the dataset
     dataset_labels = np.zeros(1000, dtype=int)
@@ -546,7 +639,7 @@ def get_train_samples(root_address, image_size, train_images_extension):
         current_address = os.path.join(root_address, object_class)
 
         # Get the list of images in the current folder
-        current_image_list = [image_name for image_name in os.listdir(current_address) if (image_name[-(len(train_images_extension) + 1)] == '.' + train_images_extension)]
+        current_image_list = [image_name for image_name in os.listdir(current_address) if (image_name[-(len(train_images_extension) + 1):] == '.' + train_images_extension)]
 
         # Increment the object number if there is any train image here
         if len(current_image_list) != 0:
