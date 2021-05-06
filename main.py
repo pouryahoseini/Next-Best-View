33e97dc82ce31d5909219bfffa51e0acdea80be3
Pourya Hoseini <shoseinialinodehi@ucsd.edu>
2021-05-05 20:14:37 -0700

    Main function now contains the following: 1- Creating a next best view instance 2- training the vision model 3- generating scores and classification results from different sides of images 4- evaluate the next best view method

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-import src.next_best_view as nbv
+from src import next_best_view as nbv
 
 
+# Create an instance of next best view class
+next_best_view = nbv.NextBestView()
+
+# Train the vision classifiers
+next_best_view.train_vision()
+
+# Generate scores and probabilities of each frontal, side, and tile images
+next_best_view.generate_scores()
+
+# Run next best view and evaluate it
+next_best_view.evaluate_next_best_view()
